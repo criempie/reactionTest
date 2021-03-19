f7f22e84e98454dbbaab8849fa16cbf732328047
criempie <56305535+criempie@users.noreply.github.com>
2021-03-19 19:58:05 +0500

    add fullscreen, statistics

--- index.js
+++ index.js
@@ -1,81 +1,61 @@
 let videoPath = "./tests/";
 let videoName = "PsySample";
-let fps = 50;
+let fps = "50";
 let format = "mp4";
 
-const pathBuilder = (n) => videoPath + videoName + "_" + fps + "_" + n + "." + format;
+let timer1;
+let vid_no = 0;
 
-let videoElements;
-let buttons;
+let counter = 0;
+let maxCount = 5;
 
-let timer;
-let clicks = 0;
-let maxClicks = 3;
+let diffs = [];
 
-let times = [];
-let whenHappend = [];
-let whenRecorded = [];
+const pathBuilder = (n) => videoPath + videoName + n + "_" + fps + "." + format;
+let vidos = [
+	"PsySample_50_1.mp4",
+	"PsySample_60_1.mp4"
+	]
 
-function playVideo() {
-    videoElements[0].attr("src", pathBuilder(1));
-    $("#video1").trigger("play")
+function setVideo(n) {
+	// timer1 = performance.now();
+	setTimeout(() => {
+		$("#video").attr("src", videoPath + vidos[n] + "?t=" + Date.now());
+	}, 100);
 }
 
 $(document).ready(() => {
-    videoElements = [
-        $("#video1"),
-        // $("#video2"),
-    ];
+	videoElement = document.getElementById("video");
 
-    buttons = {
-        start: $("#start"),
-        set50fps: $("#set50fps"),
-        set60fps: $("#set60fps"),
-    };
-    
-    buttons.set50fps.on("click", () => {fps = 50});
-    buttons.set60fps.on("click", () => {fps = 60});
-    // buttons.start.on("click", playVideo)
+    $("#start").on("click", () => {
+        $("#start").remove();
+		document.documentElement.requestFullscreen();
+		setVideo(vid_no);
+	})
 
-    videoElements[0].on("canplay", r => {
-        // setTimeout(() => {
-        //     starts.push(r.timeStamp)
-        // }, 100);
-        timer = r.timeStamp;
+	$("#video").on("loadeddata", () => {
+		timer1 = performance.now();
+		$("#video").trigger("play");
+	})
 
-        // if (clicks < maxClicks) setTimeout(() => {
-        //     $(".videoHolder").click();
-        // }, 500)
-        
-    });
+    $("#video").on("ended", () => {
+        let reactionTime = performance.now() - timer1; 
+		// console.log(vidos[vid_no] + " " + reactionTime)
+		// vid_no = (vid_no + 1 ) % (vidos.length);
+		diffs.push(reactionTime);
+		counter++;
 
-    // $(".videoHolder").on("click", r => {
-    //     if (clicks < maxClicks) {
-    //         times.push(performance.now() - timer);
-    //         clicks++;
-    //     } else {
-    //         console.log(times)
-    //     }
-        
-    // });
+		if (counter < maxCount) {
+			setVideo(vid_no);
+		} else {
+			document.exitFullscreen();
+			$(".videoHolder").remove();
+			$(".container").append('<div class="resultContainer"></div>')
+			diffs.map(e => {
+				$(".resultContainer").append("<span>" + e + "</span>")
+			})
+			
+		}
+    })
+})
 
-    videoElements[0].on("ended", r => {
-        // setTimeout(() => {
-        //     ends.push(r.timeStamp)
-        // }, 100);
-        console.log("diff: ", r.timeStamp - timer);
-
-        // if (clicks >= maxClicks) {
-        //     $(".videoHolder").attr("style", "display: none")
-        // } else {
-        //     playVideo()
-        // }
-
-        // setTimeout(() => {
-        //     i++;
-        // }, 100);
-        playVideo()
-    });
-    playVideo();
-
-});
